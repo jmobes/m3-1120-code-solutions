a81677be307b8d51f6cb42bd7cd48f2f4eee0aa2
Joseph Moberly <jmobes15@gmail.com>
2020-11-24 20:32:13 -0800

    changed react functional component using hook into a class component

--- react-hot-button-component/src/HotButton.jsx
+++ react-hot-button-component/src/HotButton.jsx
@@ -1,34 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const HotButton = () => {
-  const [count, setCount] = useState(0);
-  const [backgroundColor, setBackgroundColor] = useState(0);
-  const [fontColor, setFontColor] = useState('#000');
-  const colorsArray = ['rgb(64,0,119)', 'rgb(114,62,169)', 'rgb(243,86,92)', 'rgb(255,177,96)', 'rgb(248,255,0)', 'rgb(255,255,255)'];
+class HotButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      backgroundColor: null,
+      fontColor: '#000'
+    };
+    this.colors = ['rgb(64,0,119)', 'rgb(114,62,169)', 'rgb(243,86,92)', 'rgb(255,177,96)', 'rgb(248,255,0)', 'rgb(255,255,255)'];
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  handleClick() {
+    this.setState({ count: this.state.count + 1 }, () => {
+      if (this.state.count > 18) {
+        this.setState({ backgroundColor: '#fff' });
+      }
+      if (this.state.count && this.state.count % 3 === 0) {
+        this.setState({ backgroundColor: this.colors[this.state.count / 3 - 1] });
+      }
+      if (this.state.count >= 3 && this.state.count < 12) {
+        this.setState({ fontColor: '#fff' });
+      } else {
+        this.setState({ fontColor: '#000' });
+      }
+    });
+  }
 
-  useEffect(() => {
-    if (count && count <= 18 && count % 3 === 0) {
-      setBackgroundColor(colorsArray[count / 3 - 1]);
-    }
-    if (count >= 3 && count <= 9) {
-      setFontColor('#fff');
-    } else if (count >= 12) {
-      setFontColor('#000');
-    } else {
-      setFontColor('#000');
-    }
-  });
-
-  return (
-    <div className="button__container">
-      <button className="button" style={{ backgroundColor: backgroundColor, color: fontColor }} onClick={handleClick}>Hot Button</button>
-      <p className="count">Click Count: {count}</p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" style={{ backgroundColor: this.state.backgroundColor, color: this.state.fontColor }} onClick={this.handleClick}>Hot Button</button>
+        <p className="count">Click Count: {this.state.count}</p>
+      </div>
+    );
+  }
+}
 
 export default HotButton;
