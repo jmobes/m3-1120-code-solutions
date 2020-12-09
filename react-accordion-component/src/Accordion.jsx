9de665d4cd974e4abf894b20b6f8319120b5c2cd
Joseph Moberly <jmobes15@gmail.com>
2020-12-09 01:08:17 -0800

    simplified code so that the values are dynamic and not hard coded.

--- react-accordion-component/src/Accordion.jsx
+++ react-accordion-component/src/Accordion.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class Accordion extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       open: false,
       lastClicked: null
@@ -22,54 +22,17 @@ class Accordion extends React.Component {
   }
 
   render() {
-    let htmlClass = '';
-    if (this.state.open && this.state.lastClicked === 'headerHTML') {
-      htmlClass = 'info';
-    } else {
-      htmlClass = 'info hidden';
-    }
-    let cssClass = '';
-    if (this.state.open && this.state.lastClicked === 'headerCSS') {
-      cssClass = 'info';
-    } else {
-      cssClass = 'info hidden';
-    }
-    let jsClass = '';
-    if (this.state.open && this.state.lastClicked === 'headerJS') {
-      jsClass = 'info';
-    } else {
-      jsClass = 'info hidden';
-    }
-
+    const topics = this.props.topics;
     return (
       <div className="accordion">
-        <div className="html">
-          <h2 className="headerHTML" onClick={this.handleClick}>Hypertext Markup Language</h2>
-          <p className={htmlClass}>
-            Hypertext Markup Language (HTML) is the standard markup language
-            for creating web pages and web applications. With Cascading Style
-            Sheets(CSS) and JavaScript, it forms a triad of cornerstone
-            technologies for the World Wide Web.
-          </p>
-        </div>
-        <div className="css">
-          <h2 className="headerCSS" onClick={this.handleClick}>Cascading Style Sheets</h2>
-          <p className={cssClass}>
-            Cascading Style Sheets (CSS) is a style sheet language used for
-            describing the presentation of a document written in a markup language
-            like HTML. CSS is a cornerstone technology of the World Wide Web alongside
-            HTML and JavaScript.
-          </p>
-        </div>
-        <div className="js">
-          <h2 className="headerJS" onClick={this.handleClick}>JavaScript</h2>
-          <p className={jsClass}>
-            JavaScript, often abbreviated JS, is a high-level, interpreted programming
-            language that conforms to the ECMAScript specification. JavaScript has
-            curly-bracket syntax, dynamic typing, prototype-based object-orientation and
-            first-class functions.
-          </p>
-        </div>
+        {topics.map(topic => {
+          return (
+            <div className={topic.class.main} key={topic.id}>
+              <h2 className={topic.class.header} onClick={this.handleClick}>{topic.title}</h2>
+              <p className={this.state.open && this.state.lastClicked === topic.class.header ? 'info' : 'info hidden'}>{topic.content}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 
 import Accordion from './Accordion';
 
+const topics = [
+  {
+    id: 1,
+    title: 'Hypertext Markup Language',
+    content: 'Hypertext Markup Language (HTML) is the standard markup languagefor creating web pages and web applications. With Cascading StyleSheets(CSS) and JavaScript, it forms a triad of cornerstonetechnologies for the World Wide Web.',
+    class: { main: 'html', header: 'headerHTML' }
+  },
+  {
+    id: 2,
+    title: 'Cascading Style Sheets',
+    content: 'Cascading Style Sheets (CSS) is a style sheet language used fordescribing the presentation of a document written in a markup languagelike HTML. CSS is a cornerstone technology of the World Wide Web alongsideHTML and JavaScript.',
+    class: { main: 'css', header: 'headerCSS' }
+  },
+  {
+    id: 3,
+    title: 'JavaScript',
+    content: 'JavaScript, often abbreviated JS, is a high-level, interpreted programminglanguage that conforms to the ECMAScript specification. JavaScript hascurly-bracket syntax, dynamic typing, prototype-based object-orientation andfirst-class functions.',
+    class: { main: 'js', header: 'headerJS' }
+  }
+];
+
 const App = () => {
   return (
     <div className="app">
-      <Accordion />
+      <Accordion topics={topics} />
     </div>
   );
 };
